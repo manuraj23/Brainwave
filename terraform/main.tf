d24f74bcc716d365b96780d785b2bd4d00830de7
manuraj23 <manuraj082004@gmail.com>
2025-11-15 20:00:47 +0530

    Test 14

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "app_server" {
   ami           = "ami-0cae6d6fe6048ca2c" # Amazon Linux 2
   instance_type = "t2.micro"
-  key_name      = var.key_name
+  key_name      = var.react
 
   tags = {
     Name = "ReactAppServer"
