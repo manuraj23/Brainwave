2d5d89fb2969256c6ec8b1f5df5a63bcd4300287
manuraj23 <manuraj082004@gmail.com>
2025-11-15 20:28:49 +0530

    Test 18

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,3 +17,22 @@ resource "aws_instance" "app_server" {
     }
   }
 }
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound from anywhere (use a narrower CIDR in production)"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
