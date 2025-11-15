b4ec21c45d52a4711f02f2989add83c01569ca63
manuraj23 <manuraj082004@gmail.com>
2025-11-15 19:55:21 +0530

    Test 13

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "app_server" {
-  ami           = "ami-03695d52f0d883f65" # Amazon Linux 2
+  ami           = "ami-0cae6d6fe6048ca2c" # Amazon Linux 2
   instance_type = "t2.micro"
   key_name      = var.key_name
 
